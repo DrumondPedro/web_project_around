0e7ae0b1174ecd078f3de08444dc315533594623
pedrumond <pe.drumond97@gmail.com>
2024-04-08 15:55:48 -0300

    feat: add functionality to open and close popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
 let editor = document.querySelector(".editor");
 let profileEditButton = document.querySelector(".profile__edit-button");
-let editoCloseButton = document.querySelector(".editor__close-button");
-
-function handlePopupDisplay() {}
-
-editoCloseButton.addEventListener("click", handlePopupDisplay);
-
+let editorCloseButton = document.querySelector(".editor__close-button");
 let formElement = document.querySelector(".form");
-
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about");
+let name = formElement.querySelector(".form__input_name");
+let about = formElement.querySelector(".form__input_about");
+
+function handlePopupDisplay() {
+  editor.classList.toggle("editor_visible");
+  name.value = profileName.textContent;
+  about.value = profileAbout.textContent;
+}
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  let name = formElement.querySelector(".form__input_name");
-  let about = formElement.querySelector(".form__input_about");
   profileName.textContent = name.value;
   profileAbout.textContent = about.value;
+  handlePopupDisplay();
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+profileEditButton.addEventListener("click", handlePopupDisplay);
+editorCloseButton.addEventListener("click", handlePopupDisplay);
