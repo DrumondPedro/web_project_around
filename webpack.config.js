5ecffe947694cb644bd77af7e116473a8b580e98
pedrumond <pe.drumond97@gmail.com>
2024-09-19 11:10:00 -0300

    feat: add configuration for favicon in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      favicon: "./src/favicon.png",
     }),
     new MiniCssExtractPlugin(),
   ],
